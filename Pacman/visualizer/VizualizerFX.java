3d7b8f866ef8e7235f90011c1a270765891e8fbd
Adam Niemiec <72918433+Deutscher862@users.noreply.github.com>
2021-01-30 21:58:45 +0100

    init

---
+++ Pacman/visualizer/VizualizerFX.java
@@ -0,0 +1,10 @@
+package Pacman.visualizer;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class VizualizerFX {
+}
